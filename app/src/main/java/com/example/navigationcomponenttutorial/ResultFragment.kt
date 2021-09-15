b90060332c727e219577a7225453895459429d40
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2021-09-15 20:43:06 +0700

    add result fragment

---
+++ app/src/main/java/com/example/navigationcomponenttutorial/ResultFragment.kt
@@ -0,0 +1,25 @@
+package com.example.navigationcomponenttutorial
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_result.view.*
+
+class ResultFragment : Fragment() {
+
+    private val args by navArgs<ResultFragmentArgs>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view =  inflater.inflate(R.layout.fragment_result, container, false)
+
+        view.tvResult.text = "FirstName : ${args.currentUser.firstName}\n" +
+                "LastName : ${args.currentUser.lastName}"
+        return  view
+    }
+}
